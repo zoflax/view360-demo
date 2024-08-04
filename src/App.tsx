8d18cc5fae376ef92f66dfd16b7e9545002624fb
Austin Craig <avstincraig@gmail.com>
2024-08-04 17:10:51 +1000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { useMemo } from "react";
+import View360, { EquirectProjection } from "@egjs/react-view360";
+import "@egjs/react-view360/css/view360.min.css";
+
+export default () => {
+  const projection = useMemo(() => new EquirectProjection({
+    src: "src/assets/sample.jpg"
+  }), []);
+
+  return <View360 className="is-16by9" projection={projection} />
+}
